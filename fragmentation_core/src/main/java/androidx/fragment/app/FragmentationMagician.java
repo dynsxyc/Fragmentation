4ec1cf6d1f7a02058934d8201a6e0c39d45ed1fd
dynsxyc <dynsxyc@163.com>
2019-05-06 14:36:10 +0800

    Androidx 适配 bug修改

--- fragmentation_core/src/main/java/androidx/fragment/app/FragmentationMagician.java
+++ fragmentation_core/src/main/java/androidx/fragment/app/FragmentationMagician.java
@@ -1,11 +1,10 @@
 package androidx.fragment.app;
 
 
-import android.util.SparseArray;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -158,15 +157,18 @@ public class FragmentationMagician {
     }
 
     @SuppressWarnings("unchecked")
-    private static List<Fragment> getActiveList(SparseArray<Fragment> active) {
+    private static List<Fragment> getActiveList(HashMap<String, Fragment> active) {
         if (active == null) {
             return Collections.EMPTY_LIST;
         }
         final int count = active.size();
         ArrayList<Fragment> fragments = new ArrayList<>(count);
-        for (int i = 0; i < count; i++) {
-            fragments.add(active.valueAt(i));
+        for (String s : active.keySet()) {
+            fragments.add(active.get(s));
         }
+//        for (int i = 0; i < count; i++) {
+//            fragments.add(active.get(i));
+//        }
         return fragments;
     }
 
